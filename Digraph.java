f3a0b7ae505ce8ed932f0ad4f034b73a9dfd33db
Shashank Hegde <shashankvhegde@gmail.com>
2022-01-26 21:01:48 -0800

    formatting changes

--- Digraph.java
+++ Digraph.java
@@ -17,23 +17,23 @@ class DirectedEdge{
 
 public class Digraph{
 	private int V;
-    private List<DirectedEdge>[] adj;
+    	private List<DirectedEdge>[] adj;
     
-    public Digraph(int V){
-        this.V = V;
-        this.adj = (ArrayList<DirectedEdge>[]) new ArrayList[V];
-        for(int i = 0;i<V;i++){
-            adj[i] = new ArrayList<DirectedEdge>();
-        }
-    }
-	
-    public int getNoVertices() {
-    	return V;
-    }
-    public void addEdge(int u, int v) {
+	public Digraph(int V){
+		this.V = V;
+		this.adj = (ArrayList<DirectedEdge>[]) new ArrayList[V];
+		for(int i = 0;i<V;i++){
+		    adj[i] = new ArrayList<DirectedEdge>();
+		}
+	}
+
+	public int getNoVertices() {
+		return V;
+	}
+	public void addEdge(int u, int v) {
 		adj[u].add(new DirectedEdge(u, v));
 	}
-	
+
 	public List<Integer> getNeighbors(int vertex){
 		List<Integer> neighbors = new ArrayList<>();
 		for(DirectedEdge edge: adj[vertex]) {
@@ -41,16 +41,16 @@ public class Digraph{
 		}
 		return neighbors;
 	}
-	
-	
+
+
 	public void print(){
 	    for(int i = 0;i<V;i++){
-	        System.out.print(i + ": ");
-	        for(int j = 0;j<adj[i].size();j++){
-	            System.out.print(adj[i].get(j) + " ");
-	        }
-	        System.out.println();
+		System.out.print(i + ": ");
+		for(int j = 0;j<adj[i].size();j++){
+		    System.out.print(adj[i].get(j) + " ");
+		}
+		System.out.println();
 	    }
 	}
-	
+
 }
